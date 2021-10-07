e1cb72c9e53f39e4caadb9287e9358684486cc5d
Faizan Hussain <faizanhussain1601@gmai.com>
2021-10-07 13:35:53 +0530

    Unnecessary comments deleted

--- app/src/main/java/com/example/newsupdates/ApiInterface.java
+++ app/src/main/java/com/example/newsupdates/ApiInterface.java
@@ -13,13 +13,5 @@ public interface ApiInterface {
     Call<ResponseNews> getNews(@Query("country") String country,
                                      @Query("category") String category,
                                      @Query("apiKey") String apiKey);
-//    @GET("entertainment")
-//    Call<ResponseNews> getEntertainment();
-//    @GET("business")
-//    Call<ResponseNews> getBusiness();
-//    @GET("sports")
-//    Call<ResponseNews> getSports();
-//    @GET("technology")
-//    Call<ResponseNews> getTechnology();
+
 }
-//top-headlines?country=in&apiKey=9b4e99737a544c5aad6fc8f6be3c5f99&category=
